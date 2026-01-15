3ba96b6eaf89578da8fbbf547f5a3125a4b06447
spring <utimezgd@gmail.com>
2026-01-16 03:31:57 +0900

    first project

---
+++ MemoBom/src/main/java/com/utime/memoBom/MemoBomApplication.java
@@ -0,0 +1,13 @@
+package com.utime.memoBom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoBomApplication.class, args);
+	}
+
+}
