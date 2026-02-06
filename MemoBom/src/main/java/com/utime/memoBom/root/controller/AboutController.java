d061c230028d1e18f6711f5c07232d3ac1c4fa7d
utime <utime@pointpark.com>
2026-02-06 13:02:24 +0900

    라이센스, topic color 수정.

--- MemoBom/src/main/java/com/utime/memoBom/root/controller/AboutController.java
+++ MemoBom/src/main/java/com/utime/memoBom/root/controller/AboutController.java
@@ -1,8 +1,11 @@
 package com.utime.memoBom.root.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.utime.memoBom.common.vo.AppDefine;
 
 @Controller
 @RequestMapping("About")
@@ -14,13 +17,29 @@ public class AboutController {
     }
 	
 	@GetMapping(path = "Opensource.html")
-    public String opensource() {
+    public String opensource(ModelMap model) {
+	    model.addAttribute(AppDefine.KeyShowFooter, false);
+	    model.addAttribute(AppDefine.KeyLoadScript, false );
 		return "About/Opensource";
     }
 	
 	@GetMapping(path = "License.html")
     public String License() {
 		return "About/License";
+    }
+	
+	@GetMapping(path = "Privacy_Policy.html")
+    public String PrivacyPolicy(ModelMap model) {
+	    model.addAttribute(AppDefine.KeyShowFooter, false);
+	    model.addAttribute(AppDefine.KeyLoadScript, false );
+		return "About/Privacy_Policy";
+    }
+	
+	@GetMapping(path = "Terms_of_Service.html")
+    public String TermsOfService(ModelMap model) {
+	    model.addAttribute(AppDefine.KeyShowFooter, false);
+	    model.addAttribute(AppDefine.KeyLoadScript, false );
+		return "About/Terms_of_Service";
     }
 
 	@GetMapping(path = "Application.html")
